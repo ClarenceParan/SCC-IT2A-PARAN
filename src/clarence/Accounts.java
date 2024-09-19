8dc30130992ac0dbf88b7afd17345cc00a9a1bf7
ClarenceParan <clarenceparan1@gmail.com>
2024-09-19 08:05:51 -0700

    Add files via upload

--- src/clarence/Accounts.java
+++ src/clarence/Accounts.java
@@ -1,60 +1,26 @@
 
-package clarence;
+package javaapplication1;
 
-public class Accounts {
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String username;
-    private String password;
- 
-     
-    public Account(int id,String firstName, String lastName,String email, String username, String password){
-         this.id = id;
-         this.firstName = firstName;
-         this.lastName = lastName;
-         this.email = email;
-         this.username = username;
-         setPassword(password);
-         
-     }
-     public int getId(){
-        return id;
-     }
-     public String getFirstName(){
-         return firstName;
-     }
-     public String getLastName(){
-         return lastName;
-}  
-     public String getEmail(){
-         return email;
-     }
-     public String getUsername(){
-         return username;
-     }
-     public String getPassword(){
-         return password;
-     }
-      public void setPassword(String password){
-          validatePassword(password);
-          this.password = password;
-      }    
-      private void validatePassword(String password){
-          if (password.length() < 8){
-              System.out.println("Error: Password must be at least 8 characters long.");
-              return;
-                 
-        }
-        if(!password.matches(",*[A-Z],*") || !password.matches(",*[a-z],")){
-         System.out.println("Error: Password must contain at least one uppercase and one lowercase letter,");
-         return;
-     }
-        if(!password.matches(".*\\d,*")){
-            System.out.println("Error: Password must contain at least one number,");
-          return;
-        }
-     }
-     
+class Accounts {
+
+    Accounts(int numberOfAccounts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Accounts(int numberOfAccounts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void viewAccounts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean isPasswordValid(String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean addAccount(Account newAccount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
